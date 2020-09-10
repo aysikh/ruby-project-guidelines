5d002285d2096971dc1815158ee65682c730fff6
Aymes <aysikh@MacBook-Pro.fios-router.home>
2020-09-10 14:21:06 -0400

    working on cli

--- db/migrate/002_create_customers.rb
+++ db/migrate/002_create_customers.rb
@@ -3,7 +3,7 @@ class CreateCustomers < ActiveRecord::Migration[4.2]
   def change
     create_table :customers do |t|
       t.string :name
-      t.string :favorite_drink
+      t.string :drinks
       t.boolean :is_over_21
     end
   end
--- db/migrate/004_create_bar.rb
+++ db/migrate/004_create_bar.rb
@@ -3,9 +3,11 @@ class CreateBar < ActiveRecord::Migration[4.2]
   def change
     create_table :bar do |t|
       t.integer :customer_id
-      t.integer :drink_id
+      t.integer :drinks_id
     end
   end
 
 
 end
+
+# bar 1 = drink_id, customer_id 
