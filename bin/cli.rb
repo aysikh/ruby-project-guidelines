f2c24fb63482f3d28d62b320e01f86e62abd0675
Aymes <aysikh@MacBook-Pro.fios-router.home>
2020-09-10 12:29:57 -0400

    Updated CLI.rb

---
+++ bin/cli.rb
@@ -0,0 +1,95 @@
+require_relative '../config/environment'
+
+# ABLE TO CREATE A NEW BARTENDER
+print "Welcome to the app. Please enter your name: " 
+username = gets.chomp
+print "Years of experience? "
+experience = gets.chomp
+new_bartender = Bartender.new(name: username, years_of_experience: experience)
+# new_bartender.save
+
+# if user_input.downcase == "back"
+#   main_prompt
+# end
+
+
+puts "Hello #{new_bartender.name}! Are you ready to start bartending?"
+puts ""
+puts "'MENU' to see a list of available drinks at the bar"
+puts "'CUSTOMERS' to view current customers at the bar"
+puts "'CREATE NEW CUSTOMER' to create a new customer profile"
+puts "'CREATE NEW DRINK' to create a new drink at the bar"
+puts "'UPDATE CUSTOMER' to update a customer's profile"
+puts "'DELETE CUSTOMER' to delete a customer's profile"
+puts "or 'EXIT' to exit the application"
+
+
+
+  usage = true
+  while usage == true do 
+    user_input = gets.chomp
+  if user_input.downcase == "exit"
+    puts "Goodbye!"
+    break
+  end
+
+  if user_input.downcase == "create new customer"
+    puts "What is the customer's name? "
+    customer_name = gets.chomp
+    puts "True or False: Is #{customer_name} over 21?"
+    customer_legal = gets.chomp
+    puts "What is #{customer_name}'s favorite drink?"
+    customer_drink = gets.chomp
+    #how to raise error if customer is ordering alcoholic bev.
+    new_customer = Customer.new(name: customer_name, favorite_drink: customer_drink, is_over_21: customer_legal)
+    # new_customer.save
+    puts "Nice! #{customer_name} has been added."
+    puts "#{customer_name.upcase} IS READY TO GIT LIT!!!"
+  end
+
+  if user_input.downcase == "create new drink"
+    puts "What is the name of your drink?"
+    drink_name = gets.chomp
+    puts "What will be the price for #{drink_name}?"
+    drink_price = gets.chomp
+    puts "Does #{drink_name} contain liquor? If so, what kind? If not, leave blank."
+    drink_liquor = gets.chomp
+    puts "Does #{drink_name} comtain any extra ingredients? If so, what kind? If not, leave blank." 
+    drink_ingredients = gets.chomp
+    new_drink = Drink.new(name: drink_name, price: drink_price, liquor: drink_liquor, ingredients: drink_ingredients)
+    # new_drink.save
+    puts "#{drink_name} has been added to the menu!"
+  end
+
+  if user_input.downcase == "customers" 
+    puts Customer.all.pluck(:name)
+  end
+
+  if user_input.downcase == "menu"
+    puts Drink.all.pluck(:name)
+  end
+
+  if user_input.downcase == "update customer"
+    puts "What is the name of the customer you would like to edit?"
+    current_name = gets.chomp
+    updated = Customer.find_by name: "#{current_name}"
+    puts "What is the new name?"
+    update_name = gets.chomp
+    updated.update name: "#{update_name}"
+    puts "Name updated"
+    updated.save
+    
+
+  end
+
+  #   puts "True or False: is #{update_name} at a legal age to drink?"
+  #   update_legal = gets.chomp
+  #   Customer.update (name: "#{update_name}" , is_over_21: "#{update_legal}" 
+  #   puts "Is #{update_name} "
+
+  # end
+
+  # if user_input.downcase == "delete"
+
+
+end
